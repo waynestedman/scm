881ece2be9396df38e5efd9e798a9704e778e9f0
Wayne Stedman <wstedman@dhccare.com>
2018-05-16 17:25:10 -0700

    initial commit

---
+++ src/js/olive-slick.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $('.ingredientsCarousel').slick({
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
---
+++ src/js/server-ver.js
@@ -0,0 +1,4 @@
+<script type="text/javascript" xml="space">// <![CDATA[
+
+
+]]></script>
