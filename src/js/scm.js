ea46ef9326ef765fd34dc1df9caf1acf0866d996
Wayne Stedman <wstedman@dhccare.com>
2018-05-24 17:37:23 -0700

    images updated; prepped video

--- src/js/scm.js
+++ src/js/scm.js
@@ -31,4 +31,11 @@ $(document).ready(function(){
       }
     ]
   });
-});
+
+  var videoModal = $('#video').modal();
+  $('#openVideo').on('click', function(e) {
+    e.preventDefault();
+    videoModal.modal('openModal');
+   $("#simple-iframe").attr('src', "https://fast.wistia.net/embed/iframe/ozmj3dq5tp?videoFoam=true&autoplay=1");
+  });
+}); // doc ready
