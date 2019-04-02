3d823591bfaf6f1af4145b268270b5c242a34fed
Wayne Stedman <wstedman@dhccare.com>
2019-04-01 17:10:38 -0700

    begin UK version
    begin UK version

--- src/js/scm-video.js
+++ src/js/scm-video.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
-  var videoModal = $('#video').modal();
-
-  $('#openVideo').on('click', function(e) {
-      e.preventDefault();
-      videoModal.modal('openModal');
-  });
+  // var videoModal = $('#video').modal();
+  //
+  // $('#openVideo').on('click', function(e) {
+  //     e.preventDefault();
+  //     videoModal.modal('openModal');
+  // });
+  $('#openVideo').on('click', function() {
+    $( "#video" ).dialog({
+      modal: true,
+      buttons: {
+        Ok: function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+  }
 }); // doc ready
